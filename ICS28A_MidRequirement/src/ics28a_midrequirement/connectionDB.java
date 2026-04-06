43365563b4d16c9fbf98fcbecda89c53c3df25d0
mikay <michaelladrenojo>
2026-04-06 13:43:26 +0800

    added a function

--- ICS28A_MidRequirement/src/ics28a_midrequirement/connectionDB.java
+++ ICS28A_MidRequirement/src/ics28a_midrequirement/connectionDB.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ics28a_midrequirement;
 
-/**
- *
- * @author Acer
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class connectionDB {
-    
+  
+   
 }
