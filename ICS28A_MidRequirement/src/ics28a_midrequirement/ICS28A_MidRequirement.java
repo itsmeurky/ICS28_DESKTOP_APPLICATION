f2a1d432e00422ba82eacf4a97f1f52f4869c66c
mikay <michaelladrenojo@gmail.com>
2026-04-08 11:34:54 +0800

    first commit

---
+++ ICS28A_MidRequirement/src/ics28a_midrequirement/ICS28A_MidRequirement.java
@@ -0,0 +1,16 @@
+
+package ics28a_midrequirement;
+
+
+public class ICS28A_MidRequirement {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+       // Login LoginFrame = new login();
+        //LoginFrame.setVisible(true);
+        //LoginFrame.pack();
+        //LoginFrame.setLocationRelativeTo(null); 
+    }
+    
+}
